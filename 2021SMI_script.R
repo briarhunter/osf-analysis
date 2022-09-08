21e9a952a4d60e23c34ab040a3e0e0a1b3b345a9
Briar Hunter <briarhunts@gmail.com>
2022-09-08 18:46:01 -0400

    SMI calculations done

--- 2021SMI_script.R
+++ 2021SMI_script.R
@@ -35,6 +35,12 @@ head(wild_SMI)
 view(wild_SMI)
 dim(wild_SMI) #502 rows, 5 columns
 str(wild_SMI)
+wild_SMI$frog_id <- as.factor(wild_SMI$frog_id) #as factor. 490 levels (i.e. individual frogs)
+wild_SMI$pop <- as.factor(wild_SMI$pop) #as factor. 6 levels 
+#should not have 6 levels. need to trim white space from "MS "
+wild_SMI <- wild_SMI %>% 
+  mutate(pop = str_trim(pop, side = "both"))
+levels(wild_SMI$pop) #now has only 5 levels: "CH" "MS" "MT" "MV" "ST"
 summary(wild_SMI)
 
 prepo <-read.csv("prepost_SMI.csv", header = TRUE) #have now added calculated svl (using linear regressions below)
@@ -103,8 +109,8 @@ shapiro.test(prepo$mass_pre20) #normal
 shapiro.test(prepo$length_pre20)
 shapiro.test(prepo$svl_pre20) #add calculated svl values to check for normalcy
 shapiro.test(prepo$mass_post21)
-shapiro.test(prepo$length_post21)
-shapiro.test(prepo$svl_post21)
+shapiro.test(prepo$length_post21) #same as below
+shapiro.test(prepo$svl_post21) #normal
 shapiro.test(prepo$mass_pre_21) #normal
 shapiro.test(prepo$svl_pre21) #nearly normal but not quite
 
@@ -180,16 +186,6 @@ boot.ci(boot_out <- pomass_boot,
         type = c("norm", "basic", "perc", "bca"))
 
 #filter out NAs
-po_2021 <- prepo %>% 
-  filter(!is.na(svl_post21))
-
-posvl_boot <- boot(po_2021$svl_post21, my_mean, 1000) 
-posvl_boot
-plot(posvl_boot)
-boot.ci(boot_out <- posvl_boot,
-        type = c("norm", "basic", "perc", "bca"))
-
-#filter out NAs
 pre_2021 <- prepo %>% 
   filter(!is.na(svl_pre21))
 
@@ -208,6 +204,18 @@ left <- prebru_m_mean - prebru_m_error
 right <- prebru_m_mean + prebru_m_error
 print(c(left,right)) #(41.74126 50.05024)
 
+#filter out NAs
+po_2021 <- prepo %>% 
+  filter(!is.na(svl_post21))
+
+pobru_svl_mean <- mean(po_2021$svl_post21)
+pobru_svl_mean #35.59268
+pobru_svl_error <- qnorm(0.975)*sd(po_2021$svl_post21)/sqrt(length(po_2021$svl_post21))
+pobru_svl_error
+left <- pobru_svl_mean - pobru_svl_error
+right <- pobru_svl_mean + pobru_svl_error
+print(c(left,right)) #(32.58684, 38.59852)
+
 prebru_mean <- mean(pre_2021$mass_pre_21) #use same NA filtered out dataset as svl_pre21
 prebru_mean #52.5812
 prebru_error <- qnorm(0.975)*sd(pre_2021$mass_pre_21)/sqrt(length(pre_2021$mass_pre_21))
@@ -216,7 +224,9 @@ left <- prebru_mean - prebru_error
 right <- prebru_mean + prebru_error
 print(c(left,right)) #(49.10911 56.05329)
 
+##############################################################
 ####linear regressions to predict SVL for VanAqua SUL values
+##############################################################
 SULreg <- read.csv("SULregression.csv", header = TRUE) #only VanAqua frogs
 head(SULreg)
 view(SULreg)
@@ -279,33 +289,32 @@ predSVL <- predSVL %>%
   mutate(SUL21_SVL = predicted21) #added column of predicted 2021 SUL values from VanAqua
 view(predSVL)
 
-
-####calculate some SMI values - ref. Chen-Pan Liao R code inspired by Peig & Green (2009)
-
+##############################################################
 #linear regressions of mass by length
-
 plot(mass_pre20 ~ svl_pre20, data = prepo, main = "mass by svl pre-brumation 2020")
-
-ggplot(data = prepo, aes(svl_pre20, mass_pre20, colour = pop))+
-  geom_point(size = 2)+ #no obvious outliers
-  geom_line(aes(svl_pre20, mass_pre20), data = lmSVL1)
-  labs(title = "pre-brumation 2020 mass by svl", x = "svl (mm)", y = "mass (g)") #add a line of best fit?
-
-
-
 plot(mass_post21 ~ svl_post21, data = prepo, main = "mass by svl post-brumation 2021")
 plot(mass_pre_21 ~ svl_pre21, data = prepo, main = "mass by svl pre-brumation 2021")
+plot(avg_mass ~ avg_SVL, data = wild_SMI, main = "mass by svl for wild frogs")
 
 ###########################
 #Use steph's code 
 ###########################
 
 str(prepo)
-#subset prepo data frame by treatment
+#subset prepo data frame by populations: GVZ, VA
 prepo.GVZ <- subset(prepo, pop == "GVZ")
 str(prepo.GVZ)
 prepo.VA <- subset(prepo, pop == "VA")
 str(prepo.VA)
+
+str(wild_SMI) #n=502
+#subset wild data by populations
+wild_SMI_CH <- subset(wild_SMI, pop =="CH") #n=7
+wild_SMI_MS <- subset(wild_SMI, pop =="MS") #n=155
+wild_SMI_MT <- subset(wild_SMI, pop =="MT") #n=1
+wild_SMI_MV <- subset(wild_SMI, pop =="MV") #n=336
+wild_SMI_ST <- subset(wild_SMI, pop =="ST") #n=3
+
 
 ### Mass (dependant) vs length (independent)
 ############################ Pre-brumation (i.e. Nov) 2020
@@ -366,8 +375,8 @@ abline(lm(mass_post21 ~ svl_post21, data = prepo.GVZ), col = "blue")
 summary(lm(mass_post21 ~ svl_post21, data = prepo.GVZ))
 abline(lm(mass_post21 ~ svl_post21, data = prepo.VA), col = "green")
 summary(lm(mass_post21 ~ svl_post21, data = prepo.VA))
-legend("topright", title = "Regression lines", c("n=55, Adjusted R-squared: 0.04873, p-value: 0.0576",
-                                                "n=40, Adjusted R-squared: -0.02128, p-value: 0.6675",
+legend("topleft", title = "Regression lines", c("n=55, Adjusted R-squared: 0.4238, p-value: 4.462e-08",
+                                                "n=40, Adjusted R-squared: 0.2616, p-value: 0.0004406",
                                                 "n=15, Adjusted R-squared: 0.7145, p-value: 4.422e-05"),
        fill = c('black', 'blue', 'green'), cex = 0.6)
 title("Post-brumation (Mar) 2021")
@@ -379,6 +388,8 @@ prepo.GVZ$log.svl_post21 = log(prepo.GVZ$svl_post21)
 prepo.VA$log.svl_post21 = log(prepo.VA$svl_post21)
 prepo.GVZ$log.mass_post21 = log(prepo.GVZ$mass_post21)
 prepo.VA$log.mass_post21 = log(prepo.VA$mass_post21)
+wild_SMI$log.avg_mass = log(wild_SMI$avg_mass)
+wild_SMI$log.avg_SVL = log(wild_SMI$avg_SVL)
 
 ### Log Mass vs Log Length for 
 ############################ Post-brumation (i.e. Mar) 2021
@@ -393,8 +404,8 @@ abline(lm(log.mass_post21 ~ log.svl_post21, data = prepo.GVZ), col = "blue")
 summary(lm(log.mass_post21 ~ log.svl_post21, data = prepo.GVZ))
 abline(lm(log.mass_post21 ~ log.svl_post21, data = prepo.VA), col = "green")
 summary(lm(log.mass_post21 ~ log.svl_post21, data = prepo.VA))
-legend("topright", title = "Regression lines", c("n=55, Adjusted R-squared: 0.08053, p-value: 0.02025",
-                                                    "n=40, Adjusted R-squared: -0.02392, p-value: 0.7672",
+legend("topleft", title = "Regression lines", c("n=55, Adjusted R-squared: 0.4555, p-value: 9.62e-09",
+                                                    "n=40, Adjusted R-squared: 0.2462, p-value: 0.000668",
                                                     "n=15, Adjusted R-squared: 0.7341, p-value: 2.761e-05"),
        fill = c('black', 'blue', 'green'), cex = 0.6)
 title("Post-brumation (Mar) 2021")
@@ -446,6 +457,69 @@ legend("bottomright", title = "Regression lines", c("n=75, Adjusted R-squared: 0
        fill = c('black', 'blue', 'green'), cex = 0.6)
 title("Pre-brumation (Nov) 2021")
 
+# Mass (dependant) vs length (independent)
+############################ Wild Frog Data - full dataset (from 2012-2022*)
+plot(wild_SMI$avg_SVL, wild_SMI$avg_mass, pch = 16, cex = 1.0,
+     col = (c('blue', 'green', 'purple', 'orange', 'red')[as.numeric(wild_SMI$pop)]),
+     xlab = "snout-vent length (mm)", ylab = "mass (g)")
+legend("bottomright", title = "Population", c("Chaplin", "Maria Slough", "Mountain Slough", "Morris Valley", "Semmihault"),
+       fill = c('blue', 'green', 'purple', 'orange', 'red'), cex = 0.6)
+abline(lm(avg_mass ~ avg_SVL, data = wild_SMI), col = "black")
+lm(avg_mass ~ avg_SVL, data = wild_SMI)
+summary(lm(avg_mass ~ avg_SVL, data = wild_SMI))
+abline(lm(avg_mass ~ avg_SVL, data = wild_SMI_CH), col = "blue")
+summary(lm(avg_mass ~ avg_SVL, data = wild_SMI_CH))
+abline(lm(avg_mass ~ avg_SVL, data = wild_SMI_MS), col = "green")
+summary(lm(avg_mass ~ avg_SVL, data = wild_SMI_MS))
+abline(lm(avg_mass ~ avg_SVL, data = wild_SMI_MV), col = "orange")
+summary(lm(avg_mass ~ avg_SVL, data = wild_SMI_MV))
+abline(lm(avg_mass ~ avg_SVL, data = wild_SMI_ST), col = "red")
+summary(lm(avg_mass ~ avg_SVL, data = wild_SMI_ST))
+#no abline for MT pop because only 1 observation
+legend("topright", title = "Regression lines", c("n=502, Adjusted R-squared: 0.5563, p-value: 2.2e-16",
+                                                    "n=7, Adjusted R-squared: 0.3608, p-value: 0.0904",
+                                                    "n=155, Adjusted R-squared: 0.6485, p-value: 2.2e-16",
+                                                    "n=336, Adjusted R-squared: 0.5205, p-value: 2.2e-16",
+                                                    "n=3, Adjusted R-squared: 0.7963, p-value: 0.2068"),
+       fill = c('black', 'blue', 'green', 'orange', 'red'), cex = 0.45)
+title("Full Wild M by L")
+
+#Log-transformation
+wild_SMI$log.avg_SVL = log(wild_SMI$avg_SVL)
+wild_SMI$log.avg_mass = log(wild_SMI$avg_mass)
+wild_SMI.GVZ$log.avg_SVL = log(wild_SMI.GVZ$avg_SVL)
+wild_SMI.VA$log.avg_SVL = log(wild_SMI.VA$avg_SVL)
+wild_SMI.GVZ$log.avg_mass = log(wild_SMI.GVZ$avg_mass)
+wild_SMI.VA$log.avg_mass = log(wild_SMI.VA$avg_mass)
+
+# Log Mass vs Log Length for 
+############################ Wild Frog Data - full dataset (from 2012-2022*)
+plot(wild_SMI$log.avg_SVL, wild_SMI$log.avg_mass, pch = 16, cex = 1.0,
+     col = (c('blue', 'green', 'purple', 'orange', 'red')[as.numeric(wild_SMI$pop)]),
+     xlab = "snout-vent length (mm)", ylab = "mass (g)")
+legend("topleft", title = "Population", c("Chaplin", "Maria Slough", "Mountain Slough", "Morris Valley", "Semmihault"),
+       fill = c('blue', 'green', 'purple', 'orange', 'red'), cex = 0.6)
+abline(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI), col = "black")
+lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI)
+summary(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI))
+abline(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_CH), col = "blue")
+summary(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_CH))
+abline(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_MS), col = "green")
+summary(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_MS))
+abline(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_MV), col = "orange")
+summary(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_MV))
+abline(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_ST), col = "red")
+summary(lm(log.avg_mass ~ log.avg_SVL, data = wild_SMI_ST))
+#no abline for MT pop because only 1 observation
+legend("bottomright", title = "Regression lines", c("n=502, Adjusted R-squared: 0.5931, p-value: 2.2e-16",
+                                                 "n=7, Adjusted R-squared: 0.2854, p-value: 0.1247",
+                                                 "n=155, Adjusted R-squared: 0.6651, p-value: 2.2e-16",
+                                                 "n=336, Adjusted R-squared: 0.5663, p-value: 2.2e-16",
+                                                 "n=3, Adjusted R-squared: 0.7893, p-value: 0.2105"),
+       fill = c('black', 'blue', 'green', 'orange', 'red'), cex = 0.5)
+title("Log of M by L Full Wild dataset")
+
+
 ################################################################################
 # Calculate scaled mass index (SMI)
 ################################################################################
@@ -460,3 +534,128 @@ title("Pre-brumation (Nov) 2021")
 #         bsma is the scaling exponent: calculated indirectly by dividing the slope from an OLS regression (bOLS) by the Pearson’s correlation coefficient r (LaBarbera 1989), or directly using online software (Bohonak and van der Linde 2004).
 
 # calculate bsma manually (described in Pieg & Green 2009 p.1886)
+#for Wild frogs - using wild allometric growth as standard for scaling exponent
+lm(wild_SMI$log.avg_mass ~ wild_SMI$log.avg_SVL) #using log
+slope <- 2.336 #from variable above
+cor.test(wild_SMI$log.avg_SVL, wild_SMI$log.avg_mass, method = c("pearson")) #using log again 
+pcorr <- 0.7706459 #from above (Pearson's product-moment correlation)
+bsma <- slope / pcorr #manual calculation of bsma (scaling exponent)
+bsma #3.031224
+
+#calculate bsma again but this time automated using smatr() - Standardized Major Axis
+library(smatr)
+sma(wild_SMI$log.avg_mass ~ wild_SMI$log.avg_SVL) #use log #the slope of the estimate is the bsma 
+bsma <- 3.031213
+
+##calculate bsma for each population (i.e. VA and GVZ) 
+##### should each population have their own scaling exponent ?
+bsma_GVZ <- sma(prepo.GVZ$log.mass_pre20 ~ prepo.GVZ$log.svl_pre20) #bsma = 3.183815
+bsma_VA <- sma(prepo.VA$log.mass_pre20 ~ prepo.VA$log.svl_pre20) #bsma = 3.615024
+
+#calculate Lo
+Lo <- mean(wild_SMI$avg_SVL, na.rm = TRUE)
+Lo #71.37092
+
+Lo.GVZ <- mean(prepo.GVZ$svl_pre20, na.rm = TRUE)
+Lo.GVZ #67.71429
+
+Lo.VA <- mean(prepo.VA$svl_pre20, na.rm = TRUE)
+Lo.VA #64.59044
+
+#######create function for ScaledMassIndex using ^Mi = Mi ((Lo/Li)^bsma)
+##using bsma from MbyL regression of wild frog data
+ScaledMassIndex <-
+  function(x, y) {
+    y * ((Lo / x) ^ bsma)
+  }
+
+wild_SMI$SMI <- ScaledMassIndex(wild_SMI$avg_SVL, wild_SMI$avg_mass)
+head(wild_SMI)
+
+#calculate SMI for GVZ and VA data in "prepo" data frame
+prepo$SMIpre20 <- ScaledMassIndex(prepo$svl_pre20, prepo$mass_pre20)
+prepo$SMIpo21 <- ScaledMassIndex(prepo$svl_post21, prepo$mass_post21)
+prepo$SMIpre21 <- ScaledMassIndex(prepo$svl_pre21, prepo$mass_pre_21)
+view(prepo)
+
+#plot SMI
+plot(wild_SMI$pop, wild_SMI$SMI, xlab = "Population", ylab = "Scaled Mass Index (g)", 
+     main = "Scaled Mass Index for wild frog populations",
+     col = (c("blue", "green", "purple", "orange", "red")))
+
+plot(prepo$pop, prepo$SMIpre20, xlab = "Population", ylab = "Scaled Mass Index (g)",
+     main = "SMI for zoos pre-brumation (Nov) 2020",
+     col = (c("blue", "green")))
+plot(prepo$pop, prepo$SMIpo21, xlab = "Population", ylab = "Scaled Mass Index (g)",
+     main = "SMI for zoos post-brumation (Mar) 2021",
+     col = (c("blue", "green")))
+plot(prepo$pop, prepo$SMIpre21, xlab = "Population", ylab = "Scaled Mass Index (g)",
+     main = "SMI for zoos pre-brumation (Nov) 2021",
+     col = (c("blue", "green")))
+
+#nicer plots
+ggplot(data = wild_SMI, aes(x = pop, y = SMI, fill=factor(pop, labels = c("Chaplin", "Maria Slough", "Mountain Slough",
+                                                                          "Morris Valley", "Semmihault"))))+
+  geom_boxplot(show.legend = FALSE)+
+  labs(x = "Population", y = "Scaled Mass Index (g)", title = "SMI for wild populations")+
+  theme_classic()+
+  scale_fill_manual(values = c("blue", "green", "purple", "orange", "red"))+
+  theme(legend.title = element_blank())+
+  scale_x_discrete(labels=c("CH" = "Chaplin", "MS" = "Maria Slough", "MT" = "Mountain", "MV" = "Morris Valley", "ST" = "Semmihault"))+
+  theme(text = element_text(family = "Arial"))+
+  theme(text = element_text(size = 12))
+
+ggplot(data = prepo, aes(x = pop, y = SMIpre20, fill=factor(pop, labels = c("GVZ", "VA"))))+
+  geom_boxplot(show.legend = FALSE)+
+  labs(x = "Population", y = "Scaled Mass Index (g)", title = "SMI in zoos pre-brumation 2020")+
+  theme_classic()+
+  scale_fill_manual(values = c("blue", "green"))+
+  theme(legend.title = element_blank())+
+  scale_x_discrete(labels=c("GVZ" = "Greater Vancouver Zoo", "VA" = "Vancouer Aquarium"))+
+  theme(text = element_text(family = "Arial"))+
+  theme(text = element_text(size = 12))
+
+ggplot(data = prepo, aes(x = pop, y = SMIpo21, fill=factor(pop, labels = c("GVZ", "VA"))))+
+  geom_boxplot(show.legend = FALSE)+
+  labs(x = "Population", y = "Scaled Mass Index (g)", title = "SMI in zoos post-brumation 2021")+
+  theme_classic()+
+  scale_fill_manual(values = c("blue", "green"))+
+  theme(legend.title = element_blank())+
+  scale_x_discrete(labels=c("GVZ" = "Greater Vancouver Zoo", "VA" = "Vancouer Aquarium"))+
+  theme(text = element_text(family = "Arial"))+
+  theme(text = element_text(size = 12))
+
+ggplot(data = prepo, aes(x = pop, y = SMIpre21, fill=factor(pop, labels = c("GVZ", "VA"))))+
+  geom_boxplot(show.legend = FALSE)+
+  labs(x = "Population", y = "Scaled Mass Index (g)", title = "SMI in zoos pre-brumation 2021")+
+  theme_classic()+
+  scale_fill_manual(values = c("blue", "green"))+
+  theme(legend.title = element_blank())+
+  scale_x_discrete(labels=c("GVZ" = "Greater Vancouver Zoo", "VA" = "Vancouer Aquarium"))+
+  theme(text = element_text(family = "Arial"))+
+  theme(text = element_text(size = 12))
+
+
+
+
+###############change data frame to stacked - columns for season (pre-brumate, post-brumate), WC (Wild or Captive)
+###will then be able to compare all on same graph using facet.by()
+
+#according to Stevens 2013 (Intermediate Statistics: A Modern Approach), normality is not required for cases of large data sets (n>30)
+#Kruskal-Wallis is an alternative option, compares medians of pops instead of means- does not require normality or homoscedasticity
+
+#do a Levene test first before ANOVA to check for homoscedasticity (equality of variance) leveneTest()
+
+####assume normality for mass and length data because most samples sizes are >30 and half the data are normal, half are not
+#check homogeneity of variances (as assumption of ANOVA)
+
+#visual check of variance via boxplot
+boxplot(mass_pre20 ~ pop, prepo)
+boxplot(svl_pre20 ~ pop, prepo)
+boxplot(mass_post21 ~ pop, prepo)
+boxplot(svl_post21 ~ pop, prepo)
+boxplot(avg_mass ~ pop, prepo)
+boxplot(svl_pre21 ~ pop, prepo)
+
+#####above boxplots etc may not be necessary. Really just comparin bsma of GVZ and VA first to see if significantly different
+
